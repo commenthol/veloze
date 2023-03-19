bd23f2217ad45f852b2ae6443766795da096d4ef
commenthol <commenthol@gmail.com>
2023-03-19 10:56:39 +0100

    test(utils/random64): increase test coverage

--- test/utils/random64.spec.js
+++ test/utils/random64.spec.js
@@ -2,6 +2,7 @@ import assert from 'assert'
 import { random64 } from '../../src/utils/random64.js'
 
 const RE = /^[0-9a-zA-Z_-]+$/
+const RE_NO_DASH = /^[0-9a-zA-Z]+$/
 
 describe('string/random64', function () {
   it('shall generate a random Id', function () {
@@ -10,9 +11,15 @@ describe('string/random64', function () {
     assert(RE.test(id))
   })
 
-  it('shall generate a random Id with length 40', function () {
-    const id = random64(40)
-    assert.strictEqual(id.length, 40)
+  it('shall generate a random Id with length 256', function () {
+    const id = random64(256)
+    assert.strictEqual(id.length, 256)
     assert(RE.test(id))
+  })
+
+  it('shall generate a random Id without dashes and length 256', function () {
+    const id = random64(256, true)
+    assert.strictEqual(id.length, 256)
+    assert(RE_NO_DASH.test(id))
   })
 })
