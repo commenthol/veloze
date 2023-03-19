128f5e9165276315e9f9525c61992a58af95072c
commenthol <commenthol@gmail.com>
2023-03-19 10:57:34 +0100

    docs(HttpError): document parameters

--- src/HttpError.js
+++ src/HttpError.js
@@ -2,9 +2,9 @@ import * as http from 'node:http'
 
 export class HttpError extends Error {
   /**
-   * @param {number} status
-   * @param {string} [message]
-   * @param {Error} [err]
+   * @param {number} status HTTP status code for response
+   * @param {string} [message] error message for response
+   * @param {Error} [err] internal error cause; is logged in finalHandler; use this for any internal error
    */
   constructor (status, message, err) {
     const _message = message || http.STATUS_CODES[status] || 'general error'
--- types/HttpError.d.ts
+++ types/HttpError.d.ts
@@ -1,8 +1,8 @@
 export class HttpError extends Error {
     /**
-     * @param {number} status
-     * @param {string} [message]
-     * @param {Error} [err]
+     * @param {number} status HTTP status code for response
+     * @param {string} [message] error message for response
+     * @param {Error} [err] internal error cause; is logged in finalHandler; use this for any internal error
      */
     constructor(status: number, message?: string | undefined, err?: Error | undefined);
     status: number;
