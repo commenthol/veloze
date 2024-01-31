a38f2db0e6e12a1aff8d57f90e2ec07e8743de00
commenthol <commenthol@gmail.com>
2024-01-31 05:13:49 +0100

    chore: bump dependencies

--- types/utils/ms.d.ts
+++ types/utils/ms.d.ts
@@ -1,6 +1,6 @@
 /**
  * @param {number|string|undefined} value
- * @param {boolean} inSeconds if `true` convert `value` to seconds; Only valid if value is string
+ * @param {boolean} [inSeconds] if `true` convert `value` to seconds; Only valid if value is string
  * @returns {number|undefined}
  */
-export function ms(value: number | string | undefined, inSeconds: boolean): number | undefined;
+export function ms(value: number | string | undefined, inSeconds?: boolean | undefined): number | undefined;
